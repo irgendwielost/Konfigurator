69d6ebe3f7d0b27fcddfa3e9119fd83f3e9ca2ee
irgendwielost <fabian.conrad0304@gmail.com>
2022-01-10 01:12:54 +0100

    Added ChangePassword Window

---
+++ Konfigurator/Windows/ChangePassword.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using Konfigurator.Logic.Models.Employee;
+
+namespace Konfigurator.Windows
+{
+    public partial class ChangePassword : Window
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangePasswordButton(object sender, RoutedEventArgs e)
+        {
+            var id = idText.Text;
+            var oldPw = oldPassword.Password;
+            var newPw = newPassword.Password;
+
+            if (EmployeeDB.ChangeEmployeePassword(Int32.Parse(id), oldPw, newPw))
+            {
+                this.Close();
+            }
+        }
+    }
+}
